80292eaca9d9aaa8000e6bf5aa1ea2553e86e7fd
MASHRAFI47 <mbngms@gmail.com>
2024-06-05 02:21:16 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+
+//dotenv
+require('dotenv').config();
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken');
+
+
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+//jwt
+
+//form
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use(cookieParser())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mealLounge:.codeworld101.@cluster0.iduz7rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d'
+            })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            }).send({ success: true })
+        })
+
+        app.get('/logout', async (req, res) => {
+            try {
+                res.clearCookie('token', {
+                    maxAge: 0,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                }).send({ success: true })
+            } catch (error) {
+                res.status(500).send(error)
+            }
+        })
+
+
+
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Meal Lounge server is running")
+})
+
+module.exports = app;
